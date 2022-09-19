5d34c627d6ea7292e3c26095a14c259a93ac2553
shang <shizhongshang@cmail.carleton.ca>
2022-09-19 16:38:32 -0400

    add & remove function

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,15 +16,18 @@ public class AddressBook {
     private BuddyInfo buddy;
     private ArrayList<BuddyInfo> buddies;
 
-    public void AddBuddy(){
+    public void addBuddy(BuddyInfo buddy){
         buddies.add(buddy);
     }
 
-    public void removeBuddy(){
+    public void removeBuddy(BuddyInfo buddy){
         buddies.remove(buddy);
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
