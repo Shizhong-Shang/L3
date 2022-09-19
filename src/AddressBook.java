246ff29f52ff73f7ce7e1351a3506d96ba9c4299
shang <shizhongshang@cmail.carleton.ca>
2022-09-19 16:22:32 -0400

    add main function

---
+++ src/AddressBook.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2008-2024: Shizhong Shang
+ * Project: LAB1
+ *
+ * @className: AddressBook
+ * @Description: TODO
+ * @version: v1.17.0
+ * @author: SHIZHONG SHANG
+ * @date: 2022-09-19 2:26 p.m.
+ */
+
+public class AddressBook {
+    private BuddyInfo buddy;
+
+    public void AddBuddy(){
+
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
